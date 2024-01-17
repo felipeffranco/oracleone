a69a1b1c8da289ea4a60ebc509ac97306b0acbd6
felipeffranco <felipeffranco@outlook.com>
2024-01-17 16:30:57 -0300

    logica fds

---
+++ logica-js-fds/index.js
@@ -0,0 +1,7 @@
+let diaDaSemana = prompt('Digite o dia da semana');
+
+if(diaDaSemana === "Sábado" && diaDaSemana === "Domingo") {
+  alert("Bom fim de semana!");
+} else {
+  alert("Boa semana!");
+}
