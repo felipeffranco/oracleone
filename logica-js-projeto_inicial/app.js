bbcd7ba2daba9cfb9e75cb2083ea477440ffb572
felipeffranco <felipeffranco@outlook.com>
2024-01-18 20:21:26 -0300

    projeto acertar numero ok

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,21 +1,25 @@
 alert('Boas vindas ao jogo do número secreto');
-
-let numeroSecreto = 4;
-console.log(numeroSecreto);
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto)
 let chute;
+let tentativas = 1;
 
-white (chute != numeroSecreto) {
+while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     if (chute == numeroSecreto) {
-        alert(`Acertou o número secreto ${numeroSecreto}`);
+       break;
     } else {
-        if(chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else { 
-            alert(`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
         }
+        tentativas++
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
 
 
 
