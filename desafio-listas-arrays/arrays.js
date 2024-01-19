6196cf6f437d4d0a36db318b30ca9c8b609087ef
felipeffranco <felipeffranco@outlook.com>
2024-01-18 23:26:26 -0300

    desafio arrays and jogo numero secreto done

---
+++ desafio-listas-arrays/arrays.js
@@ -0,0 +1,11 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('Python');
+
+let names = ['Felipe', 'Nayra', 'Franco'];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[names.length - 1]);
